9ba3350c8437ba3cd64164e7029576b8bdae36e1
Leon Si <leonzalion@gmail.com>
2020-11-04 20:48:32 -0500

    cleaning config.h

--- config.h
+++ config.h
@@ -94,33 +94,33 @@ static const char *termcmd[]  = { "st", NULL };
 #include "movestack.c"
 static Key keys[] = {
         /* modifier							key		function		argument */
-        { MODKEY,				47,		swapfocus,		{.i = -1 } },      // s
-		{ MODKEY|ShiftMask,	47,		swapmaster,		{.i = -1 } },      // s
-		{ MODKEY,				57,		togglebar,		{0} },             // b
-        { MODKEY,				54,		focusstack,		{.i = +1 } },      // j
-        { MODKEY,				55,		focusstack,		{.i = -1 } },      // k
-        { MODKEY|ShiftMask,	54,		movestack,		{.i = +1 } }, // j
-        { MODKEY|ShiftMask,	55,		movestack,		{.i = -1 } }, // k
-        { MODKEY,				42,		incnmaster,		{.i = +1 } },      // i
-        { MODKEY,				43,		incnmaster,		{.i = -1 } },      // d
-        { MODKEY,				44,		setmfact,		{.f = -0.05} },    // h
-        { MODKEY,				33,		setmfact,		{.f = +0.05} },    // l
-        { MODKEY|ShiftMask,	44,		setcfact,		{.f = -0.25} }, // h
-        { MODKEY|ShiftMask,	33,		setcfact,		{.f = +0.25} }, // l
-        { MODKEY|ShiftMask,	39,		setcfact,		{.f =  0.00} }, // o
-        { MODKEY|ShiftMask,	53,		killclient,		{0} },             // q
-        { MODKEY,				58,		focusmaster,	{0} }, // m
-        { MODKEY|ShiftMask,	58,		zoom,			{0} }, // shift+m
-        { MODKEY|ShiftMask,	65,		togglefloating,	{0} },             // shift+space
-        { MODKEY,				16,		comboview,		{.ui = ~0 } },     // 0
-        { MODKEY|ShiftMask,	16,		combotag,		{.ui = ~0 } },     // 0
-        { MODKEY,				25,		focusmon,		{.i = -1 } },      // comma
-        { MODKEY,				26,		focusmon,		{.i = +1 } },      // period
-        { MODKEY|ShiftMask,	25,		tagmon,			{.i = -1 } },      // shift+comma
-        { MODKEY|ShiftMask,	26,		tagmon,			{.i = +1 } },      // shift+period
-        { MODKEY,				29,		fullscreen,		{0} },             // f
-        { MODKEY,				48,		togglescratch,	{0} },             // -
-		{ MODKEY|ShiftMask,	40,		quit,			{0} },             // e
+        { MODKEY,				47,		swapfocus,		{.i = -1 } },		// s
+		{ MODKEY|ShiftMask,	47,		swapmaster,		{.i = -1 } },		// shift+s
+		{ MODKEY,				57,		togglebar,		{0} },			// b
+        { MODKEY,				54,		focusstack,		{.i = +1 } },		// j
+        { MODKEY,				55,		focusstack,		{.i = -1 } },		// k
+        { MODKEY|ShiftMask,	54,		movestack,		{.i = +1 } },		// shift+j
+        { MODKEY|ShiftMask,	55,		movestack,		{.i = -1 } },		// shift+k
+        { MODKEY,				42,		incnmaster,		{.i = +1 } },		// i
+        { MODKEY,				43,		incnmaster,		{.i = -1 } },		// d
+        { MODKEY,				44,		setmfact,		{.f = -0.05} },		// h
+        { MODKEY,				33,		setmfact,		{.f = +0.05} },		// l
+        { MODKEY|ShiftMask,	44,		setcfact,		{.f = -0.25} },		// shift+h
+        { MODKEY|ShiftMask,	33,		setcfact,		{.f = +0.25} },		// shift+l
+        { MODKEY|ShiftMask,	39,		setcfact,		{.f =  0.00} },		// shift+o
+        { MODKEY|ShiftMask,	53,		killclient,		{0} },			// shift+q
+        { MODKEY,				58,		focusmaster,	{0} },			// m
+        { MODKEY|ShiftMask,	58,		zoom,			{0} },			// shift+m
+        { MODKEY|ShiftMask,	65,		togglefloating,	{0} },			// shift+space
+        { MODKEY,				16,		comboview,		{.ui = ~0 } },		// 0
+        { MODKEY|ShiftMask,	16,		combotag,		{.ui = ~0 } },		// shift+0
+        { MODKEY,				25,		focusmon,		{.i = -1 } },		// comma
+        { MODKEY,				26,		focusmon,		{.i = +1 } },		// period
+        { MODKEY|ShiftMask,	25,		tagmon,			{.i = -1 } },		// shift+comma
+        { MODKEY|ShiftMask,	26,		tagmon,			{.i = +1 } },		// shift+period
+        { MODKEY,				29,		fullscreen,		{0} },			// f
+        { MODKEY,				48,		togglescratch,	{0} },			// -
+		{ MODKEY|ShiftMask,	40,		quit,			{0} },			// e
 		TAGKEYS(                14,                    0)                 // 1
         TAGKEYS(                17,                    1)                 // 2
         TAGKEYS(                13,                    2)                 // 3
@@ -135,35 +135,35 @@ static Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        comboview,      {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        combotag,       {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	/*	click                	event mask      button		function		argument */
+	{	ClkLtSymbol,		0,		Button1,	setlayout,		{0} },
+	{	ClkLtSymbol,		0,		Button3,	setlayout,		{.v = &layouts[2]} },
+	{	ClkWinTitle,		0,		Button2,	zoom,			{0} },
+	{	ClkStatusText,		0,		Button2,	spawn,			{.v = termcmd } },
+	{	ClkClientWin,		MODKEY,	Button1,	movemouse,		{0} },
+	{	ClkClientWin,		MODKEY,	Button2,	togglefloating,	{0} },
+	{	ClkClientWin,		MODKEY,	Button1,	resizemouse,	{0} },
+	{	ClkTagBar,			0,		Button1,	comboview,		{0} },
+	{	ClkTagBar,			0,		Button3,	toggleview,		{0} },
+	{	ClkTagBar,			MODKEY,	Button1,	combotag,		{0} },
+	{	ClkTagBar,			MODKEY,	Button3,	toggletag,		{0} },
 };
 
 static const char *ipcsockpath = "/tmp/dwm.sock";
 static IPCCommand ipccommands[] = {
-  IPCCOMMAND(  comboview,           1, {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  combotag,            1, {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
-  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
-  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
-  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
-  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
-  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
-  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
+  IPCCOMMAND(	comboview,		1,	{ARG_TYPE_UINT}	),
+  IPCCOMMAND(	toggleview,		1,	{ARG_TYPE_UINT}	),
+  IPCCOMMAND(	combotag,		1,	{ARG_TYPE_UINT}	),
+  IPCCOMMAND(	toggletag,		1,	{ARG_TYPE_UINT}	),
+  IPCCOMMAND(	tagmon,			1,	{ARG_TYPE_UINT}	),
+  IPCCOMMAND(	focusmon,		1,	{ARG_TYPE_SINT}	),
+  IPCCOMMAND(	focusstack,		1,	{ARG_TYPE_SINT}	),
+  IPCCOMMAND(	zoom,			1,	{ARG_TYPE_NONE}	),
+  IPCCOMMAND(	incnmaster,		1,	{ARG_TYPE_SINT}	),
+  IPCCOMMAND(	killclient,		1,	{ARG_TYPE_SINT}	),
+  IPCCOMMAND(	togglefloating,	1,	{ARG_TYPE_NONE}	),
+  IPCCOMMAND(	setmfact,		1,	{ARG_TYPE_FLOAT}	),
+  IPCCOMMAND(	setlayoutsafe,	1,	{ARG_TYPE_PTR}	),
+  IPCCOMMAND(	quit,			1,	{ARG_TYPE_NONE}	)
 };
 
