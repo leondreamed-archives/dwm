8d94313d37e3c18502ef60fccedabc88102341f1
Leon Si <leonzalion@gmail.com>
2020-11-09 17:16:54 -0500

    cleaned up code

--- config.h
+++ config.h
@@ -23,9 +23,9 @@ static const char col_orange[]		= "#b58900";
 static const char col_urgborder[]	= "#dc322f";
 static const char *colors[][3]		= {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_orange  },
-	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+	[SchemeNorm] = {col_gray3,	col_gray1,	col_gray2},
+	[SchemeSel]  = {col_gray4,	col_cyan,	col_orange},
+	[SchemeUrg]  = {col_gray4,	col_cyan,	col_urgborder},
 };
 
 typedef struct {
@@ -50,18 +50,18 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/*		class				instance    		title		tags	mask	isfloating	isterminal		noswallow		monitor*/
-	{ "Gimp",				NULL,		NULL,	0,			1,	-1,	0,	0 },
-    { "st-256color",		NULL,   	NULL,	1 << 0,	0,	1,		0,	-1 },
-    { "qutebrowser",		NULL,		NULL,	1 << 1,	0,	-1,	0,	-1 },
-    { "jetbrains-idea",	NULL,		NULL,	1 << 2,	0,	-1,	0,	-1 },
-    { "Todoist",			NULL,		NULL,	1 << 3,	0,	-1,	0,	-1 },
-    { "Element",			NULL,		NULL,	1 << 4,	0,	1,		0,	0 },
-    { "jetbrains-clion",	NULL,		NULL,	1 << 5,	0,	-1,	0,	-1 },
-	{ "joplin",			NULL,		NULL,	1 << 6,	0,	-1,	0,	-1 },
-	{ "Slack",			NULL,		NULL,	1 << 7,	0,	-1,	0,	-1 },
-	{ NULL,				"spterm",	NULL,	SPTAG(0),1,	-1 },
-	{ NULL,				"spfm",		NULL,	SPTAG(1),1,	-1 },
-	{ NULL,				"keepassxc",NULL,	SPTAG(2),0,	-1 },
+	{"Gimp",				NULL,		NULL,	0,			1,	-1,	0,	0},
+	{"st-256color",		NULL,   	NULL,	1 << 0,	0,	1,		0,	-1},
+	{"qutebrowser",		NULL,		NULL,	1 << 1,	0,	-1,	0,	-1},
+	{"jetbrains-idea",	NULL,		NULL,	1 << 2,	0,	-1,	0,	-1},
+	{"Todoist",			NULL,		NULL,	1 << 3,	0,	-1,	0,	-1},
+	{"Element",			NULL,		NULL,	1 << 4,	0,	1,		0,	0},
+	{"jetbrains-clion",	NULL,		NULL,	1 << 5,	0,	-1,	0,	-1},
+	{"joplin",			NULL,		NULL,	1 << 6,	0,	-1,	0,	-1},
+	{"Slack",				NULL,		NULL,	1 << 7,	0,	-1,	0,	-1},
+	{NULL,				"spterm",	NULL,	SPTAG(0),1,	-1},
+	{NULL,				"spfm",		NULL,	SPTAG(1),1,	-1},
+	{NULL,				"keepassxc",NULL,	SPTAG(2),0,	-1},
 };
 
 /* layout(s) */
@@ -72,19 +72,19 @@ static const int attachbelow	= 1;	/* 1 means attach after the currently active w
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",	tile },			/* first entry is default */
-	{ "[F]",	NULL },	/* no layout function means floating behavior */
-	{ "[M]",	monocle },
-	{ "[D]",	deck },
+	{"[T]",	tile},			/* first entry is default */
+	{"[F]",	NULL},	/* no layout function means floating behavior */
+	{"[M]",	monocle},
+	{"[D]",	deck},
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,						KEY,	comboview,		{.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,			KEY,	toggleview,		{.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,				KEY,	combotag,		{.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask,	KEY,	toggletag,		{.ui = 1 << TAG} },
+	{MODKEY,						KEY,	comboview,		{.ui = 1 << TAG}}, \
+	{MODKEY|ControlMask,			KEY,	toggleview,		{.ui = 1 << TAG}}, \
+	{MODKEY|ShiftMask,				KEY,	combotag,		{.ui = 1 << TAG}}, \
+	{MODKEY|ControlMask|ShiftMask,	KEY,	toggletag,		{.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -97,35 +97,35 @@ static const char *termcmd[]  = { "st", NULL };
 #include "movestack.c"
 static Key keys[] = {
         /* modifier							key		function		argument */
-        { MODKEY,				47,		swapfocus,		{.i = -1} },		// s
-		{ MODKEY|ShiftMask,	47,		swapmaster,		{.i = -1} },		// shift+s
-		{ MODKEY,				57,		togglebar,		{0} },			// b
-        { MODKEY,				54,		focusstack,		{.i = +1} },		// j
-        { MODKEY,				55,		focusstack,		{.i = -1} },		// k
-        { MODKEY|ShiftMask,	54,		movestack,		{.i = +1} },		// shift+j
-        { MODKEY|ShiftMask,	55,		movestack,		{.i = -1} },		// shift+k
-        { MODKEY|ShiftMask,	42,		incnmaster,		{.i = +1} },		// shift+i
-        { MODKEY|ShiftMask,	43,		incnmaster,		{.i = -1} },		// shift+d
-        { MODKEY,				44,		setmfact,		{.f = -0.05} },		// h
-        { MODKEY,				33,		setmfact,		{.f = +0.05} },		// l
-        { MODKEY|ShiftMask,	44,		setcfact,		{.f = -0.25} },		// shift+h
-        { MODKEY|ShiftMask,	33,		setcfact,		{.f = +0.25} },		// shift+l
-        { MODKEY|ShiftMask,	39,		setcfact,		{.f =  0.00} },		// shift+o
-        { MODKEY|ShiftMask,	53,		killclient,		{0} },			// shift+q
-        { MODKEY,				58,		focusmaster,	{0} },			// m
-        { MODKEY|ShiftMask,	58,		zoom,			{0} },			// shift+m
-		{ MODKEY|ControlMask,	45,		setlayout,		{.v = &layouts[0]}},	// ctrl+t
-		{ MODKEY|ControlMask,	43,		setlayout,		{.v = &layouts[3]}},	// ctrl+d
-        { MODKEY|ShiftMask,	65,		togglefloating,	{0} },			// shift+space
-        { MODKEY,				16,		comboview,		{.ui = ~0} },		// 0
-        { MODKEY|ShiftMask,	16,		combotag,		{.ui = ~0 }},		// shift+0
-        { MODKEY,				25,		focusmon,		{.i = -1} },		// comma
-        { MODKEY,				26,		focusmon,		{.i = +1} },		// period
-        { MODKEY|ShiftMask,	25,		tagmon,			{.i = -1} },		// shift+comma
-        { MODKEY|ShiftMask,	26,		tagmon,			{.i = +1} },		// shift+period
-        { MODKEY,				29,		fullscreen,		{0} },			// f
-        { MODKEY,				48,		togglescratch,	{0} },			// c
-		{ MODKEY|ShiftMask,	40,		quit,			{0} },			// e
+        {MODKEY,				47,		swapfocus,		{.i = -1}},		// s
+		{MODKEY|ShiftMask,		47,		swapmaster,		{.i = -1}},		// shift+s
+		{MODKEY,				57,		togglebar,		{0}},		// b
+		{MODKEY,				54,		focusstack,		{.i = +1}},		// j
+		{MODKEY,				55,		focusstack,		{.i = -1}},		// k
+		{MODKEY|ShiftMask,		54,		movestack,		{.i = +1}},		// shift+j
+		{MODKEY|ShiftMask,		55,		movestack,		{.i = -1}},		// shift+k
+		{MODKEY|ShiftMask,		42,		incnmaster,		{.i = +1}},		// shift+i
+		{MODKEY|ShiftMask,		43,		incnmaster,		{.i = -1}},		// shift+d
+		{MODKEY,				44,		setmfact,		{.f = -0.05}},	// h
+		{MODKEY,				33,		setmfact,		{.f = +0.05}},	// l
+        {MODKEY|ShiftMask,		44,		setcfact,		{.f = -0.25}},	// shift+h
+        {MODKEY|ShiftMask,		33,		setcfact,		{.f = +0.25}},	// shift+l
+        {MODKEY|ShiftMask,		39,		setcfact,		{.f =  0.00}},	// shift+o
+        {MODKEY|ShiftMask,		53,		killclient,		{0}},		// shift+q
+        {MODKEY,				58,		focusmaster,	{0}},		// m
+        {MODKEY|ShiftMask,		58,		zoom,			{0}},		// shift+m
+		{MODKEY|ControlMask,	45,		setlayout,		{.v = &layouts[0]}},	// ctrl+t
+		{MODKEY|ControlMask,	43,		setlayout,		{.v = &layouts[3]}},	// ctrl+d
+        {MODKEY|ShiftMask,		65,		togglefloating,	{0}},		// shift+space
+        {MODKEY,				16,		comboview,		{.ui = ~0}},	// 0
+        {MODKEY|ShiftMask,		16,		combotag,		{.ui = ~0 }},	// shift+0
+        {MODKEY,				25,		focusmon,		{.i = -1}},		// comma
+        {MODKEY,				26,		focusmon,		{.i = +1}},		// period
+        {MODKEY|ShiftMask,		25,		tagmon,			{.i = -1}},		// shift+comma
+        {MODKEY|ShiftMask,		26,		tagmon,			{.i = +1}},		// shift+period
+        {MODKEY,				29,		fullscreen,		{0}},		// f
+        {MODKEY,				48,		togglescratch,	{0}},		// -
+		{MODKEY|ShiftMask,		40,		quit,			{0}},		// e
 		TAGKEYS(                14,                    0)                 // 1
         TAGKEYS(                17,                    1)                 // 2
         TAGKEYS(                13,                    2)                 // 3
@@ -142,34 +142,34 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/*	click                	event mask      button		function		argument */
-	{ ClkLtSymbol,			0,		Button1,	setlayout,		{0} },
-	{ ClkLtSymbol,			0,		Button3,	setlayout,		{.v = &layouts[2]} },
-	{ ClkWinTitle,			0,		Button2,	zoom,			{0} },
-	{ ClkStatusText,		0,		Button2,	spawn,			{.v = termcmd } },
-	{ ClkClientWin,		MODKEY,	Button1,	movemouse,		{0} },
-	{ ClkClientWin,		MODKEY,	Button2,	togglefloating,	{0} },
-	{ ClkClientWin,		MODKEY,	Button3,	resizemouse,	{0} },
-	{ ClkTagBar,			0,		Button1,	comboview,		{0} },
-	{ ClkTagBar,			0,		Button3,	toggleview,		{0} },
-	{ ClkTagBar,			MODKEY,	Button1,	combotag,		{0} },
-	{ ClkTagBar,			MODKEY,	Button3,	toggletag,		{0} },
+	{ClkLtSymbol,			0,		Button1,	setlayout,		{0}},
+	{ClkLtSymbol,			0,		Button3,	setlayout,		{.v = &layouts[2]}},
+	{ClkWinTitle,			0,		Button2,	zoom,			{0}},
+	{ClkStatusText,		0,		Button2,	spawn,			{.v = termcmd}},
+	{ClkClientWin,			MODKEY,	Button1,	movemouse,		{0}},
+	{ClkClientWin,			MODKEY,	Button2,	togglefloating,	{0}},
+	{ClkClientWin,			MODKEY,	Button3,	resizemouse,	{0}},
+	{ClkTagBar,			0,		Button1,	comboview,		{0}},
+	{ClkTagBar,			0,		Button3,	toggleview,		{0}},
+	{ClkTagBar,			MODKEY,	Button1,	combotag,		{0}},
+	{ClkTagBar,			MODKEY,	Button3,	toggletag,		{0}},
 };
 
 static const char *ipcsockpath = "/tmp/dwm.sock";
 static IPCCommand ipccommands[] = {
-  IPCCOMMAND(	comboview,		1,	{ARG_TYPE_UINT}	),
-  IPCCOMMAND(	toggleview,		1,	{ARG_TYPE_UINT}	),
-  IPCCOMMAND(	combotag,		1,	{ARG_TYPE_UINT}	),
-  IPCCOMMAND(	toggletag,		1,	{ARG_TYPE_UINT}	),
-  IPCCOMMAND(	tagmon,			1,	{ARG_TYPE_UINT}	),
-  IPCCOMMAND(	focusmon,		1,	{ARG_TYPE_SINT}	),
-  IPCCOMMAND(	focusstack,		1,	{ARG_TYPE_SINT}	),
-  IPCCOMMAND(	zoom,			1,	{ARG_TYPE_NONE}	),
-  IPCCOMMAND(	incnmaster,		1,	{ARG_TYPE_SINT}	),
-  IPCCOMMAND(	killclient,		1,	{ARG_TYPE_SINT}	),
-  IPCCOMMAND(	togglefloating,	1,	{ARG_TYPE_NONE}	),
-  IPCCOMMAND(	setmfact,		1,	{ARG_TYPE_FLOAT}	),
-  IPCCOMMAND(	setlayoutsafe,	1,	{ARG_TYPE_PTR}	),
-  IPCCOMMAND(	quit,			1,	{ARG_TYPE_NONE}	)
+  IPCCOMMAND(comboview,		1,	{ARG_TYPE_UINT}),
+  IPCCOMMAND(toggleview,	1,	{ARG_TYPE_UINT}),
+  IPCCOMMAND(combotag,		1,	{ARG_TYPE_UINT}),
+  IPCCOMMAND(toggletag,		1,	{ARG_TYPE_UINT}),
+  IPCCOMMAND(tagmon,		1,	{ARG_TYPE_UINT}),
+  IPCCOMMAND(focusmon,		1,	{ARG_TYPE_SINT}),
+  IPCCOMMAND(focusstack,	1,	{ARG_TYPE_SINT}),
+  IPCCOMMAND(zoom,			1,	{ARG_TYPE_NONE}),
+  IPCCOMMAND(incnmaster,	1,	{ARG_TYPE_SINT}),
+  IPCCOMMAND(killclient,	1,	{ARG_TYPE_SINT}),
+  IPCCOMMAND(togglefloating,1,	{ARG_TYPE_NONE}),
+  IPCCOMMAND(setmfact,		1,	{ARG_TYPE_FLOAT}),
+  IPCCOMMAND(setlayoutsafe,	1,	{ARG_TYPE_PTR}),
+  IPCCOMMAND(quit,			1,	{ARG_TYPE_NONE})
 };
 
